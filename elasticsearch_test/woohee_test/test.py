64391a27d168ac69894698fa83bd583f79d04558
epicarts <0505zxc@gmail.com>
2019-10-10 09:17:11 +0000

    woohee_test

---
+++ elasticsearch_test/woohee_test/test.py
@@ -0,0 +1,55 @@
+import json
+from elasticsearch import Elasticsearch, helpers
+
+es = Elasticsearch("http://127.0.0.1:9200/")
+print(es.info())
+b_body = {
+	"settings": {
+            "index": {
+                "analysis": {
+                    "analyzer": {
+                        "my_analyzer": {
+                            "type": "custom",
+                           
+                        }
+                    }
+                }
+            }
+        },
+		"mappings": {
+            "dictionary_datas": {
+                "properties": {
+                    "id": {
+                        "type": "long"
+                    },
+                    "title": {
+                        "type": "text",
+                        "analyzer": "my_analyzer"
+                    },
+                    "content": {
+                        "type": "text",
+                        "analyzer": "my_analyzer"
+                    }
+                }
+            }
+        }
+	}
+def make_index(es, index_name):
+    """인덱스를 신규 생성한다(존재하면 삭제 후 생성) """
+    if es.indices.exists(index=index_name):
+        es.indices.delete(index=index_name)
+    print(es.indices.create(index=index_name))
+print('\n')
+
+index_name = 'dictionary'
+make_index(es, index_name) # 상품 데이터 덩어리(인덱스)를 생성한다 
+
+with open("/workspace/test/dictionary_data.json",'r',encoding='utf-8') as json_file:
+	json_data = json.loads(json_file.read())
+	
+body = ""
+for i in json_data:
+	body = body + json.dumps({"index": {"_index":"dictionary","_type": "dictionary_datas"}}) + '\n'
+	body = body + json.dumps(i,ensure_ascii=False) + '\n'
+	
+es.bulk(body)
